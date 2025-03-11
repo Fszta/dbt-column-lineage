37c4852f68b06211b60a3c7da543aef31d518581
Fszta <antoineferszte@gmail.com>
2025-03-11 20:08:57 +0100

    remove file

--- main.py
+++
@@ -1,9 +0,0 @@
-from dbt_column_lineage.registry.registry import ModelRegistry
-
-registry = ModelRegistry(
-    catalog_path="tests/dbt_test_project/target/catalog.json",
-    manifest_path="tests/dbt_test_project/target/manifest.json"
-)
-registry.load()
-
-models = registry.get_models()
